32822d9d2bdbb6518fb16b20c8459c9084ea2a0f
Raiv <raiv.ixx@gmail.com>
2018-10-09 19:06:39 +0300

    cleanup channel config

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -25,5 +25,5 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/h
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b /etc/hyperledger/channel/mychannel.block
 # Join peer1.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" -e "peer0.org1.example.com:7051" peer1.org1.example.com peer channel join -b /etc/hyperledger/channel/mychannel.block
-channel join -b mychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b /etc/hyperledger/channel/mychannel.block
+
