d44162a2c360d1d07331b14b706fdb0bda95ee75
Raiv <raiv.ixx@gmail.com>
2018-10-11 06:17:25 +0300

    38

--- claims-server/server/fabric/fabricuser.js
+++ claims-server/server/fabric/fabricuser.js
@@ -12,13 +12,14 @@ class FabricUser{
         let fabric_client = new Fabric_Client();
         // setup the fabric network
         let channel = fabric_client.newChannel('mychannel');
-        let peer = fabric_client.newPeer('grpc://localhost:7051');
-      //  let peer2 = fabric_client.newPeer('grpc://localhost:8051');
-        
      //   channel.addPeer(peer2);
         var order = fabric_client.newOrderer('grpc://localhost:7050')
+        let peer = fabric_client.newPeer('grpc://localhost:7051');
+        let peer2 = fabric_client.newPeer('grpc://localhost:8051');
         channel.addOrderer(order);
         channel.addPeer(peer);
+        channel.addPeer(peer2);
+
         let member_user_tmp=null;
         let store_path = path.join(__dirname, '../../../claims/hfc-key-store');
         console.log('Store path:'+store_path);
